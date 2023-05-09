4d88ac1f9e84a06f98eff93a03adeadd50b96201
DOREENDAVID <doreenkinya153@gmail.com>
2023-05-09 16:42:39 -0700

    print combinations of 2 digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@
 for num1 in range(0, 9):
     for num2 in range(num1 + 1, 10):
         if num1 != 8:
-            print(f"{}{}".format(num1, num2), end=", ")
+            print("{}{}".format(num1, num2), end=", ")
         else:
             print("{}{}".format(num1, num2))
