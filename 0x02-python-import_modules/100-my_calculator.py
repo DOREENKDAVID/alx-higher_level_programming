eb7b0e3ecb032767bda27ee3680eb13270ea307e
DOREENDAVID <doreenkinya153@gmail.com>
2023-05-11 14:58:38 -0700

    pycosestyle formatting

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-   from calculator_1 import add, sub, mul, div
-   import sys
+    from calculator_1 import add, sub, mul, div
+    import sys
 
-   num_of_args = len(argv)
-   if (num_of_args != 4):
-       print ("Usage: ./100-my_calculator.py <a> <operator> <b>")
-       exit(1)
+    num_of_args = len(argv)
+    if (num_of_args != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
 
-   a = int(sys.argv[1])
-   operator = sys.agv[2]
-   b = int(sys.argc[3])
+    a = int(sys.argv[1])
+    operator = sys.agv[2]
+    b = int(sys.argc[3])
 
-   if operator  == '+':
+    if operator  == '+':
         print("{} {} {} = {}".format(a, operator, b, add(a, b)))
     elif operator == '-':
         print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
