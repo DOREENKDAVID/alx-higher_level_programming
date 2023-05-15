1137bb59e665b2643964a85c0c0869a447f0d0e9
DOREENDAVID <doreenkinya153@gmail.com>
2023-05-15 12:00:36 -0700

    linked list palindome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,55 +1,70 @@
-#include <unistd.h>
-#include <stdlib.h>
 #include "lists.h"
-#include <stdio.h>
+#include <stddef.h>
 
 /**
- * length_of_list - how long the list is
- * @head:pointer to list
- * Return: length of list
+ * @*reverse_listint - Reverses a linked list in palindome
+ * @head: Pointer to a pointer pointing to the first item in the list
+ * Return: The new head of the reversed list
  */
-int length_of_list(listint_t *head)
+
+listint_t *reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
+
+listint_t *reverse_listint(listint_t **head)
 {
-	int len;
+  listint_t *node = *head, *next, *prev = NULL;
 
-	for (len = 0; head; len++)
-		head = head->next;
-
-	return (len);
+  while (node)
+    {
+      next = node->next;
+      node->next = prev;
+      prev = node;
+      node = next;
+    }
+  *head = prev;
+  return (*head);
 }
 
 /**
- * is_palindrome - func that check if reverse is = to original
- * @head:double pointer to list
- * Return: 1 on sucess ,0 in failure
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * reverse_listint - Reverses a linked list in place
+ * @head: A pointer to the head of the linked list
+ * Return: if not a palindrome - 0. If a palindrome - 1.
  */
 
 int is_palindrome(listint_t **head)
 {
-	int i, len, half, *entire;
-	listint_t *temp = *head;
-	char *entire = NULL
-	if (!head)
-		return (1);
+	listint_t *temp, *rev, *half;
+	size_t size = 0, i;
 
-	len = length_of_list(temp);
-	//entire = malloc(sizeof(int) * len);
-	if (!entire)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 	temp = *head;
 
-	for (i = 0; temp; i++)
+	while (temp)
 	{
-		entire[i] = temp->n;
+		size++;
 		temp = temp->next;
 	}
-	half = i / 2;
+	temp = *head;
 
-	for (i = 0; i <= half; i++)
+	for (i = 0; i < (size / 2) - 1; i++)
+	temp = temp->next;
+	if ((size % 2) == 0 && temp->n != temp->next->n)
+		return (0);
+
+	temp = temp->next->next;
+	rev = reverse_listint(&temp);
+	half = rev;
+	temp = *head;
+
+	while (rev)
 	{
-		if (entire[i] != entire[len - (i + 1)])
-			return (0);
-
+	if (temp->n != rev->n)
+		return (0);
+	temp = temp->next;
+	rev = rev->next;
 	}
+	reverse_listint(&half);
 	return (1);
 }
