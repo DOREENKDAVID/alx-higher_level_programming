57cb5c85ceacb62547aa7a643b8ecdb4eeae41ee
DOREENDAVID <doreenkinya153@gmail.com>
2023-05-15 12:19:19 -0700

    variable swipe

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     num_of_elem = len(matrix[0])
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            if j < num_of_elem - 1:
-                print("{:d}".format(matrix[i][j]), end=" ")
+    for row in range(len(matrix)):
+        for column in range(len(matrix[0])):
+            if column < num_of_elem - 1:
+                print("{:d}".format(matrix[row][column]), end=" ")
             else:
-                print("{:d}".format(matrix[i][j]), end="")
+                print("{:d}".format(matrix[row][column]), end="")
         print()
