d62cc45b0edfb08be6a7043b93205aa95482515c
DOREENDAVID <doreenkinya153@gmail.com>
2023-05-17 15:29:34 -0700

    advanced tasks

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,4 +4,4 @@ def weight_average(my_list=[]):
         return 0
     score_weight = sum([num[0] * num[1] for num in my_list])
     weighted_mean = score_weight / sum([num[1] for num in my_list])
-    return result
+    return weighted_mean
