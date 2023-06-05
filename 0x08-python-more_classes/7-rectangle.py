eae3aa59b8e3c8f1565458a9d29b8ba47d2cee8e
DOREENDAVID <doreenkinya153@gmail.com>
2023-06-05 15:29:32 -0700

    Change representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -81,7 +81,7 @@ class Rectangle:
         """
         string = ""
         if self.__width != 0 and self.__height != 0:
-            string += "\n".join("#" * self.__width
+            string += "\n".join(str(self.print_symbol) * self.__width
                                 for i in range(self.__height))
         return string
 
