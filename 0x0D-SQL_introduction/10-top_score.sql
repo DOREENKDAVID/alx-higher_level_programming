ba7eee3b263f1bfdc65d4b57a746ab01583b43a8
DOREENKDAVID <doreenkinya153@gmail.com>
2023-07-18 16:01:02 -0700

    List by best

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -3,4 +3,4 @@
 -- Results should display both the score and the name (in this order)
 -- Records should be ordered by score (top first)
 
-SELECT score, name FROM second_table ODRER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
