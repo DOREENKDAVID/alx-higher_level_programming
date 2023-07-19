646c3b62d13f2e36b35ad721b89ec511b8c1e055
DOREENKDAVID <doreenkinya153@gmail.com>
2023-07-19 14:23:28 -0700

    creates the database hbtn_0d_2 and the user user_0d_2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,10 +4,13 @@
 -- The user_0d_2 password should be set to user_0d_2_pwd
 -- If the database hbtn_0d_2 already exists, your script should not fail
 -- If the user user_0d_2 already exists, your script should not fail
-CREATE DATABASE IF NOT EXISTS hbtn_od_2;
+-- creating the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
+-- creating user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
 
-GRANT SELECT ON hbtn_od_2.* TO 'user_0d_2'@'localhost';
+-- granting permissions to user_0d_2 for the database hbtn_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
