cd8cce5e7d234b471f5baa3418608bfe21eedc10
DOREENKDAVID <doreenkinya153@gmail.com>
2023-07-19 18:42:30 -0700

    creates the database hbtn_0d_usa and the table cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -13,7 +13,7 @@ USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXIST cities (
 	id INT AUTO_INCREMENT,
-	state_id INT NOT NULL;
+	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY (state_id) REFERENCES states(id) ON DELETE CASCADE);
