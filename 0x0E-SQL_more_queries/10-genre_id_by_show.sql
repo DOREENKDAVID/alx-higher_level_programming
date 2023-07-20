18ed31c38169e7e286f6f48d08ab84c1ad047af6
DOREENKDAVID <doreenkinya153@gmail.com>
2023-07-19 19:14:59 -0700

    script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -6,7 +6,7 @@
 -- The database name will be passed as an argument of the mysql command
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows_genres
+FROM tv_show_genres
 JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
