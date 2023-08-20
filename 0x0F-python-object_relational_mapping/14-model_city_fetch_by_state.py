20078aa345c2dd82b69c18841d17e3ffe9502ef2
DOREENKDAVID <doreenkinya153@gmail.com>
2023-08-20 05:17:52 -0700

    Python file that contains the class definition of a City and prints all City objects from the database hbtn_0e_14_usa:

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -5,11 +5,12 @@ script that prints all City objects from the database hbtn_0e_14_usa:
 
 
 import sqlalchemy
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from model_state import Base, State, City
+from model_state import Base, State
+from model_city import City
 from sys import argv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
