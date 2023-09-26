d6dd49d12c7ea4d3c6e94ee62e2e8a7a3f7dd378
DOREENKDAVID <doreenkinya153@gmail.com>
2023-09-26 13:39:48 -0700

    script that writes a string to a file.

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-/* script that Reads and Prints the contents of a file */
+/* script that prints the contents of a file */
 
-/*Import the 'fs' (file system) module to work with files.*/
+/* Import the 'fs' (file system) module to work with files. */
 const fs = require('fs');
-/*Get the third command-line argument file name*/
+/* Get the third command-line argument file name */
 const filename = process.argv[2];
-/*read the contents using the readline method*/
-fs.readFile(filename, 'utf8', (err, data) => {
-  if (err) console.log(err);/*print error*/
-  else console.log(data.toString());/*print contents of file*/
+const text = process.argv[3];
+/* writes the contents using the writeFile method */
+fs.writeFile(filename, text, 'utf8', (err) => {
+  if (err) console.log(err);/* print error */
 });
